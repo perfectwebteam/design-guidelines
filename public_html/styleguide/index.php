83da3cc4e16dd77642835e42176e6435204567e6
Sander Potjer <github@sanderpotjer.nl>
2022-05-20 16:26:22 +0200

    Implement twig for styleguide
    Signed-off-by: Sander Potjer <github@sanderpotjer.nl>

---
+++ public_html/styleguide/index.php
@@ -0,0 +1,10 @@
+<?php
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+require_once '../../vendor/autoload.php';
+
+$loader = new FilesystemLoader('/');
+$twig = new Environment($loader);
+
+echo $twig->render('index.twig');
