8a211cdad6a954763489b14ce84c59ad4b8fcf35
Sander Potjer <github@sanderpotjer.nl>
2022-05-23 17:04:05 +0200

    Move styleguide files to root folder
    Signed-off-by: Sander Potjer <github@sanderpotjer.nl>

--- public_html/index.php
+++ public_html/index.php
@@ -2,7 +2,7 @@
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-require_once '../../vendor/autoload.php';
+require_once '../vendor/autoload.php';
 
 $loader = new FilesystemLoader('/');
 $twig = new Environment($loader);
